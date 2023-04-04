05ab5d9e545838aaa9491c5ee64d4279279d6575
汪志鹏 <13476859295@163.com>
2023-04-04 13:17:34 +0800

    add SplitButton, StartPage components

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
+import StartPage from './pages/StartPage'
 
 function App() {
   return (
     <div className="App">
+      <StartPage />
     </div>
   );
 }
---
+++ client/src/pages/StartPage.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import SplitButton from '../components/SplitButton'
+
+const StartPage = () => {
+  return (
+    <SplitButton />
+  )
+}
+
+export default StartPage;
