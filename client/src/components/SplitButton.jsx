a6cc22d913750ae8ba35e32bebc9880eaace1c1f
汪志鹏 <13476859295@163.com>
2023-04-05 20:03:37 +0800

    add axios

--- client/src/components/SplitButton.jsx
+++ client/src/components/SplitButton.jsx
@@ -85,6 +85,7 @@ export default function SplitButton() {
                   {options.map((option, index) => (
                     <MenuItem
                       key={option}
+                      disabled={index === 2}
                       selected={index === selectedIndex}
                       onClick={(event) => handleMenuItemClick(event, index)}
                     >
--- client/src/utils/connectSQLite.js
+++ client/src/utils/connectSQLite.js
@@ -1,1 +1,35 @@
 import Axios from 'axios';
+import config_axios from '../assets/data/config_axios.json'
+
+
+function login(userType, email, password) {
+    const url = server_host + 'api/login';
+    const data = {
+      userType: userType,
+      email: email,
+      password: password
+    };
+    return axios.post(url, data)
+      .then(response => {
+        return response.data;
+      })
+      .catch(error => {
+        throw new Error(error.message);
+      });
+  }
+
+  function register(userType, email, password) {
+    const url = server_host + 'api/register';
+    const data = {
+      userType: userType,
+      email: email,
+      password: password
+    };
+    return axios.post(url, data)
+      .then(response => {
+        return response.data;
+      })
+      .catch(error => {
+        throw new Error(error.message);
+      });
+  }
