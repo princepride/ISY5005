3869cfd51efcfd83ebdd384b63e709d76a49189d
汪志鹏 <13476859295@163.com>
2023-04-07 02:05:46 +0800

    add wzp inject to all pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,44 +1,61 @@
+//import './App.css';
+//import { BrowserRouter, Routes, Route } from 'react-router-dom';
+//import { useEffect } from'react';
+//import StartPage from './pages/StartPage';
+//import LoginPage from './pages/LoginPage';
+//import FormPage from './pages/FormPage';
+//import TestPage from './pages/TestPage';
+//import { ContextProvider, useStateContext } from './contexts/ContextProvider';
+//import config_data from './assets/data/config_data.json'
+
+//function App() {
+//  useEffect(() => {
+//    window.wzp = {
+//      route: function (path) {
+//        window.location.href = 'http://localhost:3000/' + path;
+//      },
+//      click: function (selector) {
+//        document.querySelector(selector).click();
+//      },
+//      input: function (selector, text) {
+//        document.querySelector(selector).value = text;
+//      },
+//      print: () => {
+
+//      }
+//    };
+//  }, []);
+
+//  return (
+//    <div className="App">
+//      <ContextProvider>
+//          <BrowserRouter>
+//            <Routes>
+//              <Route path="/" element={(<StartPage />)} />
+//              <Route path="/login" element={(<LoginPage/>)} />
+//              <Route path="/form" element={(<FormPage />)} />
+//              <Route path="/test" element={(<TestPage />)} />
+//            </Routes>
+//          </BrowserRouter>
+//      </ContextProvider>
+//    </div>
+//  );
+//}
+
+//export default App;
+
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { useEffect } from'react';
-import StartPage from './pages/StartPage';
-import LoginPage from './pages/LoginPage';
-import FormPage from './pages/FormPage';
-import TestPage from './pages/TestPage';
 import { ContextProvider } from './contexts/ContextProvider';
-import { WZPContextProvider } from './contexts/WZPContextProvider';
-import config_data from './assets/data/config_data.json'
+import RoutePage from './pages/RoutePage';
 
 function App() {
-
-  useEffect(() => {
-    // 将wzp对象挂载到全局对象上
-    window.wzp = {
-      route: function (path) {
-        window.location.href = 'http://localhost:3000/' + path;
-      },
-      click: function (selector) {
-        document.querySelector(selector).click();
-      },
-      input: function (selector, text) {
-        document.querySelector(selector).value = text;
-      }
-    };
-  }, []);
 
   return (
     <div className="App">
       <ContextProvider>
-        <WZPContextProvider>
-          <BrowserRouter>
-            <Routes>
-              <Route path="/" element={(<StartPage />)} />
-              <Route path="/login" element={(<LoginPage/>)} />
-              <Route path="/form" element={(<FormPage />)} />
-              <Route path="/test" element={(<TestPage />)} />
-            </Routes>
-          </BrowserRouter>
-        </WZPContextProvider>
+        <RoutePage />
       </ContextProvider>
     </div>
   );
---
+++ client/src/pages/RoutePage.jsx
@@ -0,0 +1,42 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { useEffect } from'react';
+import StartPage from './StartPage';
+import LoginPage from './LoginPage';
+import FormPage from './FormPage';
+import TestPage from './TestPage';
+import { useStateContext } from '../contexts/ContextProvider';
+import config_data from '../assets/data/config_data.json'
+
+function RoutePage() {
+    const { userType } = useStateContext();
+    useEffect(() => {
+        window.wzp = {
+        route: function (path) {
+            window.location.href = 'http://localhost:3000/' + path;
+        },
+        click: function (selector) {
+            document.querySelector(selector).click();
+        },
+        input: function (selector, text) {
+            document.querySelector(selector).value = text;
+        },
+        print: () => {
+            console.log(userType);
+        }
+        };
+  }, [userType]);
+
+  return (
+    <BrowserRouter>
+        <Routes>
+            <Route path="/" element={(<StartPage />)} />
+            <Route path="/login" element={(<LoginPage/>)} />
+            <Route path="/form" element={(<FormPage />)} />
+            <Route path="/test" element={(<TestPage />)} />
+        </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default RoutePage;
+
