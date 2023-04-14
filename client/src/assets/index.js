199039a50e73274ea1df7c69ea7ff692645cb3a2
汪志鹏 <13476859295@163.com>
2023-04-14 17:58:41 +0800

    add data to index

---
+++ client/src/assets/index.js
@@ -0,0 +1,16 @@
+export {default as config_axios} from "./data/config_axios.json"
+export {default as config_data} from "./data/config_data.json"
+
+export {default as img1} from "./images/img1.jpg"
+export {default as img2} from "./images/img2.jpg"
+export {default as img3} from "./images/img3.jpg"
+export {default as img4} from "./images/img4.jpg"
+export {default as img5} from "./images/img5.jpg"
+export {default as img6} from "./images/img6.jpg"
+
+export {default as test} from "./styles/test.json"
+
+export {default as customer} from "./videos/customer.mp4"
+export {default as enterprise} from "./videos/enterprise.mp4"
+
+
--- client/src/pages/CusHomePage.jsx
+++ client/src/pages/CusHomePage.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
-import img1 from '../assets/images/img1.jpg'
-import img2 from '../assets/images/img2.jpg'
-import img3 from '../assets/images/img3.jpg'
-import img4 from '../assets/images/img4.jpg'
-import img5 from '../assets/images/img5.jpg'
-import img6 from '../assets/images/img6.jpg'
+//import img1 from '../assets/images/img1.jpg'
+//import img2 from '../assets/images/img2.jpg'
+//import img3 from '../assets/images/img3.jpg'
+//import img4 from '../assets/images/img4.jpg'
+//import img5 from '../assets/images/img5.jpg'
+//import img6 from '../assets/images/img6.jpg'
+import { img1, img2, img3, img4, img5, img6 } from "../assets"
 import BusinessCard from '../components/BusinessCard'
 
 function CusHomePage() {
